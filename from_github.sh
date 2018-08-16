65c5bacfc52587e489a45e7316a488918064adeb
Emmanuel Touzery <etouzery@gmail.com>
2018-08-16 09:33:20 +0200

    upgrade from/to github scripts

--- from_github.sh
+++ from_github.sh
@@ -1,1 +1,4 @@
+mkdir -p ~/.config/i3/
 cp ~/home/dotfiles/i3-config ~/.config/i3/config
+mkdir -p ~/.config/gsimplecal/
+cp ~/home/dotfiles/gsimplecal-config  ~/.config/gsimplecal/config
--- to_github.sh
+++ to_github.sh
@@ -1,1 +1,2 @@
 cp ~/.config/i3/config ~/home/dotfiles/i3-config
+cp ~/.config/gsimplecal/config ~/home/dotfiles/gsimplecal-config
