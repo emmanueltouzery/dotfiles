44f6514179136e14e2745cf3b5894ab49fc5abe5
Emmanuel Touzery <etouzery@gmail.com>
2019-01-30 19:57:05 +0100

    fix vscode from_github

--- from_github.sh
+++ from_github.sh
@@ -3,4 +3,5 @@ cp ~/home/dotfiles/i3-config ~/.config/i3/config
 mkdir -p ~/.config/gsimplecal/
 cp ~/home/dotfiles/gsimplecal-config  ~/.config/gsimplecal/config
 cp gnome-control-center.desktop ~/.local/share/applications
-cp vscode-settings.json ~/.config/Code/User
+mkdir -p ~/.config/Code/User
+cp vscode-settings.json ~/.config/Code/User/settings.json
