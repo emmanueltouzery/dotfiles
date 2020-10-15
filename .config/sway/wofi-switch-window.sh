f921bb62d7493b2b590eb6345e7c9e41f11f464d
emmanueltouzery <etouzery@gmail.com>
2020-10-15 16:51:01 +0200

    add wofi script

---
+++ .config/sway/wofi-switch-window.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + " " + .name)' | wofi -i -I --show dmenu  | cut -d' ' -f1 | {
+    read -r id name
+    swaymsg "[con_id=$id]" focus
+}
