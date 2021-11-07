fe6e15a4165eb0d68a9120d274c8eb1e74ac20d7
emmanueltouzery <etouzery@gmail.com>
2021-11-07 10:20:07 +0100

    more resilient, need that for the new laptop

--- .config/sway/display_volume.sh
+++ .config/sway/display_volume.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-unmuted=$(pactl list sinks | grep -c "Mute: yes")
+# unmuted=$(pactl list sinks | grep -c "Mute: yes")
+unmuted=$(pactl get-sink-mute @DEFAULT_SINK@ | grep -c "Mute: yes")
 if [ "$unmuted" = "0" ]
 then
-    pactl list sinks | grep Volume | head -n 1 | awk '{print $5}' | sed s/%// > /tmp/wobpipe
+#    pactl list sinks | grep Volume | head -n 1 | awk '{print $5}' | sed s/%// > /tmp/wobpipe
+     pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | awk '{print $5}' | sed s/%// > /tmp/wobpipe
 else
     echo 0 > /tmp/wobpipe
 fi
