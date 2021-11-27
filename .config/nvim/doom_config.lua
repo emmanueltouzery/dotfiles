c20e5dab6eb66ef92ed072aaae08ba00b6fd2ba5
Emmanuel Touzery <etouzery@gmail.com>
2021-11-27 18:10:06 +0100

    patched galaxyline to display the shortline on inactive windows too

--- .config/nvim/doom_config.lua
+++ .config/nvim/doom_config.lua
@@ -394,6 +394,7 @@ local nvim = {
         require('kommentary.config').configure_language("default", {
             prefer_single_line_comments = true,
         })
+        require('galaxyline').inactive_window_shortline = false
      end
   },
 
