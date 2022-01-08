d0081a5517272792beae4e29da9f315ae1c04821
Emmanuel Touzery <etouzery@gmail.com>
2022-01-08 15:04:48 +0100

    wofi switch window: differentiate floating+hidden windows

--- .config/sway/wofi-switch-window.sh
+++ .config/sway/wofi-switch-window.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + " " + .name + " - " + .app_id)' | wofi -i -I --show dmenu  | cut -d' ' -f1 | {
+swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + " " + .name + " - " + .app_id + " " + (if (.type == "floating_con" and (.visible | not)) then "🥷" else "" end))' | wofi -i -I --show dmenu  | cut -d' ' -f1 | {
     read -r id name
     swaymsg "[con_id=$id]" focus
 }
