810074916aa99f3baec8f7c305dded363ede45bb
Emmanuel Touzery <etouzery@gmail.com>
2022-05-17 21:05:54 +0200

    ability to jump to a file:line by pasting a file:line pattern
    will guess the project based on the projectile-project project list,
    stole some code from them. also improved some functions to copy paths --
    make sure to take into account the project root, which may not be the
    same as the relative path (eg if you have a git repo, then inside it in
    a subfolder a JS project, the root would be the JS project, not the git
    rpeo, but I might want the git repo)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -85,7 +85,7 @@ function _G.select_current_qf(also_print)
 end
 
 function _G.get_file_line()
-    local file_path = vim.fn.expand("%")
+    local file_path = cur_file_path_in_project()
     local line = vim.fn.line(".")
     return "`" .. file_path .. ":" .. line .. "`"
 end
@@ -97,7 +97,7 @@ function _G.copy_file_line()
 end
 
 function _G.get_file_line_sel()
-    local file_path = vim.fn.expand("%")
+    local file_path = cur_file_path_in_project()
     local start_line = vim.fn.getpos("v")[2]
     local end_line = vim.fn.getcurpos()[2]
     -- local start_line = vim.fn.line("'<")
@@ -109,6 +109,72 @@ function _G.copy_file_line_sel()
     local to_copy = get_file_line_sel()
     vim.cmd("let @+ = '" .. to_copy .. "'")
     print(to_copy)
+end
+
+function _G.copy_to_clipboard(to_copy)
+    vim.cmd("let @+ = '" .. to_copy .. "'")
+end
+
+-- TELESCOPE-PROJECT START
+-- lifted from https://github.com/nvim-telescope/telescope-project.nvim/blob/master/lua/telescope/_extensions/project/utils.lua
+telescope_projects_file = vim.fn.stdpath('data') .. '/telescope-projects.txt'
+
+-- Get project info for all (de)activated projects
+get_project_objects = function()
+  local projects = {}
+  for line in io.lines(telescope_projects_file) do
+    local project = parse_project_line(line)
+    table.insert(projects, project)
+  end
+  return projects
+end
+
+-- Extracts information from telescope projects line
+parse_project_line = function(line)
+  local title, path, workspace, activated = line:match("^(.-)=(.-)=(.-)=(.-)$")
+  if not workspace then
+    title, path = line:match("^(.-)=(.-)$")
+    workspace = 'w0'
+  end
+  if not activated then
+    title, path, workspace = line:match("^(.-)=(.-)=(.-)$")
+    activated = 1
+  end
+  return {
+    title = title,
+    path = path,
+    workspace = workspace,
+    activated = activated
+  }
+end
+
+-- 
+-- TELESCOPE-PROJECT END
+
+function _G.goto_fileline()
+    local input = vim.fn.input("Enter file:line please: ")
+    vim.cmd("redraw") -- https://stackoverflow.com/a/44892301/516188
+    local fname = input:match("[^:]+")
+    local line = input:gsub("[^:]+:", "")
+    for _, project in pairs(get_project_objects()) do
+        if vim.fn.filereadable(project.path .. "/" .. fname) == 1 then
+            vim.cmd(":e " .. project.path .. "/" .. fname)
+            vim.cmd(":" .. line)
+            return
+        end
+    end
+    vim.cmd("echoh WarningMsg | echo \"Can't find file in any project: " .. fname .. "\" | echoh None")
+end
+
+function _G.cur_file_path_in_project()
+    local full_path = vim.fn.expand("%:p")
+    for _, project in pairs(get_project_objects()) do
+        if full_path:match("^" .. escape_pattern(project.path)) then
+            return full_path:gsub("^" .. escape_pattern(project.path .. "/"), "")
+        end
+    end
+    -- no project that matches, return the relative path
+    return vim.fn.expand("%")
 end
 
 -- huge hack. the first time I'd open nvim-tree in doom-nvim,
